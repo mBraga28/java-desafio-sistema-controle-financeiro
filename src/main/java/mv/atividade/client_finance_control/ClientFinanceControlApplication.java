d8f765f1cc949e77935340c76930cbeee57fe7fe
Marco Braga <maboa17@gmail.com>
2024-09-08 16:01:14 -0300

    chore: initial Spring Boot project setup

---
+++ src/main/java/mv/atividade/client_finance_control/ClientFinanceControlApplication.java
@@ -0,0 +1,13 @@
+package mv.atividade.client_finance_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientFinanceControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientFinanceControlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mv/atividade/client_finance_control/ClientFinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package mv.atividade.client_finance_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientFinanceControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
